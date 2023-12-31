ebf32187aad14f258306e24155b595f91ffa964a
AnuragKush13 <anurag.kush13@gmail.com>
2023-12-31 18:42:01 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [height, setHeight] = useState(0);
+  const [weight,setWeight] = useState(0);
+  const [bmi,setBmi] = useState('');
+  const [category,setCategory] = useState('');
+
+  let calcBMI=()=>{
+    if(height == 0 || weight == 0 )
+    alert("Please enter values!");
+  else
+    setBmi((weight/(height*height)*10000).toFixed(1));
+
+  }
+
+  const resetBtn=()=>{
+    window.location.reload();
+  }
+
+  useEffect(()=>{
+    
+    
+    if(bmi<18.5)
+    setCategory("Underweight");
+  else if(bmi >= 18.5 && bmi <=24.9)
+  setCategory("Normal");
+  else if( bmi > 24.9 && bmi<=29.9)
+  setCategory("Overweight");
+else if(bmi>29.9)
+setCategory("Obesity");
+  })
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="container" >
+        <h3>Calculate you BMI</h3>
+      <p>Weight</p>
+        <input value={weight} onChange={(e)=>setWeight(e.target.value)} placeholder ="Enter weight in kg"></input>
+        
+        <p>Height</p>
+        <input value={height} onChange={(e)=>setHeight(e.target.value)} placeholder ="Enter height in cmtrs"></input>
+        <button className="calcBMI" onClick={calcBMI}> Calculate </button>
+        <button className="resetBtn" onClick={resetBtn}> Reset </button>
+        <p>{bmi} kg/m<sup>2</sup></p>
+        <p>{category}</p>
+      </div>
     </div>
   );
 }
